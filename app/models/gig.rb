f971ae9d6aa0786dc2b0a9f13b19e5b160c84f82
github username <github email address>
2020-02-12 20:30:01 -0500

    remove attending from list of required variables

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,5 +1,5 @@
 class Gig < ActiveRecord::Base
     belongs_to :member
 
-    validates_presence_of :gig_name, :date, :location, :attending
+    validates_presence_of :gig_name, :date, :location
 end
